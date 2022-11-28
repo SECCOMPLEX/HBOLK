ab131f2cfe620e0704b506bee9cba7ed4fbac0a9
SECCOMPLEX <95067541+SECCOMPLEX@users.noreply.github.com>
2022-11-28 19:36:42 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -23,7 +23,7 @@ async def start(client, message):
     if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
         buttons = [[
                        InlineKeyboardButton('🤖 Uᴘᴅᴀᴛᴇs', url='https://t.me/SECLK'),
-                InlineKeyboardButton('ℹ️ Hᴇʟᴘ ', url=f"https://t.me/{temp.U_NAME}?start=help")
+                InlineKeyboardButton('ℹ️ Hᴇʟᴘ ', url=f"https://t.me/{temp.U_NAME}?start=start")
                 ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply(script.START_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup)
