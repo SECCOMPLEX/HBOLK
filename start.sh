378595b21af890e855027503051a1e1f69978ee0
SECCOMPLEX <95067541+SECCOMPLEX@users.noreply.github.com>
2022-11-28 20:26:37 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/SECCOMPLEX/AutoFilterSECLK.git /AutoFilterSECLK
+  git clone https://github.com/SECCOMPLEX/HBOLK.git /HBOLK
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /AutoFilterSECLK
+  git clone $UPSTREAM_REPO /HBOLK
 fi
-cd /AutoFilterSECLK
+cd /HBOLK
 pip3 install -U -r requirements.txt
-echo "Starting AutoFilterSECLK...."
+echo "Starting HBOLK...."
 python3 bot.py
